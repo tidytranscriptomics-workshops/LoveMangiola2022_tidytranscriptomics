9bd61cb054bf1b188b6aedc8d7d3a79337e0057f
stemangiola <mangiolastefano@gmail.com>
2022-12-12 11:35:47 +1100

    update workshop

--- data-raw/seurat_obj.R
+++ data-raw/seurat_obj.R
@@ -8,4 +8,17 @@ seurat_obj[["SCT"]]@scale.data = seurat_obj[["SCT"]]@scale.data[c("CD3D", "TRDC"
 seurat_obj[["SCT"]]@data = seurat_obj[["SCT"]]@data[c("CD3D", "TRDC", "TRGC1", "TRGC2", "CD8A", "CD8B"),]
 DefaultAssay(seurat_obj) = "SCT"
 seurat_obj[["integrated"]] = NULL
+
+# Update treatment 
+slot(seurat_obj$SCT@SCTModel.list[[1]], 'median_umi') = median(seurat_obj$SCT@SCTModel.list[[1]]@cell.attributes$umi)
+
+seurat_obj = seurat_obj |> 
+	tidyseurat::extract(file, "sample", "../data/.*/([a-zA-Z0-9_-]+)/outs.+", remove = FALSE) |> 
+	left_join(
+		readr::read_csv("/stornext/Bioinf/data/bioinf-data/Papenfuss_lab/projects/mangiola.s/PostDoc/oligo_breast/expanded_analyses_with_control/OMBC - Sheet1.csv") |>
+			select(sample, type)
+	)  |>
+	mutate(treatment = if_else(type=="MBC", TRUE, FALSE)) |> 
+	select(-sample, -type) 
+
 save(seurat_obj , file="data/seurat_obj.rda", compress = "xz")
